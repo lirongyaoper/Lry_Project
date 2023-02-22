b652021449dee055755e5966f6f16bf9879378ad
lirongyaoper <lirongyaoper@163.com>
2023-02-22 22:00:11 +0800

    20230222001

---
+++ Python/python_library/001赋值.py
@@ -0,0 +1,48 @@
+# a = "zhangxiaoji"
+
+# def foo(b):
+#     print (">>>before id(b):",id(b))
+#     b = "lizhiwei"
+#     print (">>>after id(b):",id(b))
+# print (">>>before id(a):", id(a))
+
+# foo(a)
+# print (">>>after id(a):", id(a))
+# print(f"{a}")
+######################################################################
+
+# a = ["lirongyaoper"]
+# def foo (b):
+#     print(">>> before valve(b):",b)
+#     print(">>> before id(b):",id(b))
+#     b[0] = "lizhiwei"
+#     b.append("guoqingwei")
+#     print(">>> before valve(b):",b)    
+#     print(">>> after id (b):",id(b))
+# print(">>> before value(a):",a)
+# print(">>> before id(a):",id(a))
+# foo(a)
+# print(">>> after value(a):",a)
+# print(">>> after id(a):",id(a))
+######################################################################
+import copy
+a = "lirongyao"
+a1 =a 
+a2 = copy.copy(a)
+a = "lirongyaoper"
+
+
+b = ["lirongyao",54,32,999]
+b1 = b
+b2 = copy.copy(b)
+b[1] = "lizhaoyao"
+b[3] = "cuixiujuan"
+
+
+c = [["中国","美国","加拿大"],"习近平",["朝鲜","日本"]]
+c1 = c
+c2 = copy.copy(c)
+c[0][2] = "英国"
+c[1] = "李荣耀"
+
+
