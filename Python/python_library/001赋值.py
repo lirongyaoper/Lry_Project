478ba5e5ff240e7c8dffda3caeeceb63a810e1c1
lirongyaoper <lirongyaoper@163.com>
2023-02-25 22:05:37 +0800

    20220002250001

--- Python/python_library/001赋值.py
+++ Python/python_library/001赋值.py
@@ -25,24 +25,36 @@
 # print(">>> after value(a):",a)
 # print(">>> after id(a):",id(a))
 ######################################################################
-import copy
-a = "lirongyao"
-a1 =a 
-a2 = copy.copy(a)
-a = "lirongyaoper"
+# import copy
+# a = "lirongyao"
+# a1 =a 
+# a2 = copy.copy(a)
+# a = "lirongyaoper"
 
 
-b = ["lirongyao",54,32,999]
-b1 = b
-b2 = copy.copy(b)
-b[1] = "lizhaoyao"
-b[3] = "cuixiujuan"
+# b = ["lirongyao",54,32,999]
+# b1 = b
+# b2 = copy.copy(b)
+# b[1] = "lizhaoyao"
+# b[3] = "cuixiujuan"
 
 
-c = [["中国","美国","加拿大"],"习近平",["朝鲜","日本"]]
-c1 = c
-c2 = copy.copy(c)
-c[0][2] = "英国"
-c[1] = "李荣耀"
+# c = [["中国","美国","加拿大"],"习近平",["朝鲜","日本"]]
+# c1 = c
+# c2 = copy.copy(c)
+# c[0][2] = "英国"
+# c[1] = "李荣耀"
+
+
+a1 = "lirongyao"
+list1 = [1,2,a1,3,"yes",[4,5]]
+list2 = [6,7]
+list3 = list1 +list2
+
+list4 = list1.extend(list2)
+
+list5 = list1.append(list2)
+
+
 
 
